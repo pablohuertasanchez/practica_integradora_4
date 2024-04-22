d3f5134d34417765b25cae58f96ecda4a0b1c278
pabloPC <minisombra@gmail.com>
2024-04-22 21:09:23 +0200

    primer commit

---
+++ src/main/java/org/grupo4/practica_integradora_g4/PracticaIntegradoraG4Application.java
@@ -0,0 +1,13 @@
+package org.grupo4.practica_integradora_g4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaIntegradoraG4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaIntegradoraG4Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/grupo4/practica_integradora_g4/PracticaIntegradoraG4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grupo4.practica_integradora_g4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaIntegradoraG4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
