1a7d3b37065f7d777877524a0929a65f3bcd5b9b
LaRoca33 <arocaangel19@gmail.com>
2024-04-26 23:25:59 +0200

    Cuarto e incompleto commit login

--- src/main/java/org/grupo4/practica_integradora_g4/controller/ControladorLoginUsuario.java
+++ src/main/java/org/grupo4/practica_integradora_g4/controller/ControladorLoginUsuario.java
@@ -1,8 +1,12 @@
 package org.grupo4.practica_integradora_g4.controller;
 
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.grupo4.practica_integradora_g4.entidades.Usuario;
+import org.springframework.beans.BeanMetadataAttributeAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,22 +21,43 @@ public class ControladorLoginUsuario {
     }
 
     @PostMapping("/loginUsuario-post")
-    public String log1P(@ModelAttribute("usario") Usuario u,
+    public String log1P(@ModelAttribute("usuario") Usuario u,
                         Model modelo){
         return "redirect:/registro";
     }
 
     @GetMapping("/registroUsuario")
     public String reg1(@ModelAttribute("usuario")Usuario u,
-                       Model modelo){
+                       Model modelo,
+                       HttpSession sesion){
+
+        Usuario uS = (Usuario) sesion.getAttribute("usuario");
+        modelo.addAttribute("usuario",u);
+        if (uS !=null){
+            modelo.addAttribute("usuario",uS);
+            System.out.println(uS.toString());
+        }
 
         return "registroUsuario.html";
     }
 
     @PostMapping("/registroUsuario-post")
-    public String reg1P(@ModelAttribute("usuario")Usuario u,
-                        Model modelo){
-        return "redirect:/loginUsuario";
+    public String reg1P(@Valid @ModelAttribute("usuario")Usuario u,
+                        Model modelo,
+                        BindingResult result,
+                        HttpSession sesion){
+
+        if (result.hasErrors()){
+            modelo.addAttribute("errors",result.getAllErrors());
+            System.out.println(result.getAllErrors());
+            return "registroUsuario.html";
+        }
+
+            sesion.setAttribute("usuario",u);
+            return "redirect:/loginUsuario";
+
+
+
     }
 
 }
--- src/main/java/org/grupo4/practica_integradora_g4/entidades/Usuario.java
+++ src/main/java/org/grupo4/practica_integradora_g4/entidades/Usuario.java
@@ -2,7 +2,9 @@ package org.grupo4.practica_integradora_g4.entidades;
 
 import lombok.*;
 import org.grupo4.practica_integradora_g4.embedded.Auditoria;
+import org.grupo4.practica_integradora_g4.validaciones.ComprobarClaves;
 import org.grupo4.practica_integradora_g4.validaciones.ComprobarEmail;
+import org.grupo4.practica_integradora_g4.validaciones.ComprobarNombreEmail;
 import org.springframework.validation.annotation.Validated;
 
 import jakarta.validation.constraints.*;
@@ -13,14 +15,23 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @RequiredArgsConstructor
 @ToString
+@Data
+@Validated
+@ComprobarClaves
 public class Usuario {
     @NotNull
     @NotBlank
     @ComprobarEmail
+    @ComprobarNombreEmail
     private String email;
+    @NotNull
+    @NotBlank
+    @Size(min=6, max=12)
+    @Pattern(regexp ="(?=.*[0-9])(?=.*[a-zñ])(?=.*[A-ZÑ])(?=.*[!#$%&]).*")
     private String clave;
+    @NotNull
     private String confirmarClave;
-    private LocalDate fechaUltimaConexion;
-    private Integer numeroAccesos;
+    private LocalDate fechaUltimaConexion=LocalDate.now();
+    private Integer numeroAccesos=0;
     private Auditoria auditoria;
 }
