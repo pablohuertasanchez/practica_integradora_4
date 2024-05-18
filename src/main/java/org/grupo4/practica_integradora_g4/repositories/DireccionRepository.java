ad34c31e7a19da6d406c53e64d8385c14c7f8a79
LaRoca33 <arocaangel19@gmail.com>
2024-05-18 15:17:30 +0200

    Repositorios commit 5 Direcciones

---
+++ src/main/java/org/grupo4/practica_integradora_g4/repositories/DireccionRepository.java
@@ -0,0 +1,7 @@
+package org.grupo4.practica_integradora_g4.repositories;
+
+import org.grupo4.practica_integradora_g4.model.entidades.Direccion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DireccionRepository extends JpaRepository<Direccion,String> {
+}
